69939a161f124615f0e685cd61e98f21b438cdef
Shukurullo Meliboev <113255469+shukurullo2004@users.noreply.github.com>
2023-05-26 23:35:00 +0900

    first commit

---
+++ learning.js
@@ -0,0 +1,183 @@
+"use strict";
+// // let a = 34;
+// // let b = 56;
+// // console.log(a*b);
+alert("Salom Shukurullo, kod yozishda davom etamizmi ? let's click ok!");
+// let i = 0
+// while(i <= 10)
+// {
+//     console.log(i)
+//     i++
+// }
+
+
+// for( let i = 0; i <= 100; i++){
+//     console.log(i)
+// }
+
+
+
+
+// let i =0;
+// do {
+//     console.log(i); i++
+// }
+// while(i<20)
+
+
+
+// let answers =[]
+// let questions = [
+//     "what is your name ?",
+//     "where are you studying?",
+//     "how old are you?",
+//     "what is your dream job?"
+// ]
+// for (let i=0; i < questions.length; i++)
+// {
+//     answers[i]=prompt(questions[i])
+// }
+// console.log(answers)
+
+
+
+// let answers =[]
+// let questions = [
+//     "what is your name ?",
+//     "where are you studying?",
+//     "how old are you?",
+//     "what is your dream job?"
+// ]
+// let i = 0
+// do { answers[i]=prompt(questions[i]);
+//     console.log(answers);
+//     i++
+    
+// }
+// while (i < questions.length);
+
+
+
+// let answers =[]
+// let questions = [
+//     "what is your name ?",
+//     "where are you studying?",
+//     "how old are you?",
+//     "what is your dream job?"
+// ]
+// let i = 0;
+// while(i < questions.length){
+//     answers[i] = prompt(questions[i]); 
+//     i++ 
+// }
+// console.log(answers);
+
+
+
+
+
+// let age = +prompt("yoshingizni kiriting")
+// if(age >= 18) {
+//     alert("you can enter site")
+// }else {
+//     alert("you have to be upper of 18+")
+// }
+
+// switch(age){
+//     case 18:
+//         alert('congrulate you bro you are 18+ xixixixaxaxhohoh')
+//         break
+//     case 17:
+//         alert("your age is 17")
+//         break
+//     case 19:
+//         alert("your age is same with Shukurullo's age")
+//         break
+// }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+// const finder = (mn, mx, givenNumber) => {
+//     if(givenNumber > mn && givenNumber < mx)
+//     {
+//         console.log(`berilgan son ${givenNumber} ${mn} va ${mx} orasida`);
+//     } else{
+//         console.log(`orasida emas ${mx}`)
+//     }
+// }
+//     let mn = 23;
+//     let mx = 45;
+//     let givenNumber = 78;
+
+//     finder(mn, mx, givenNumber)
+
+
+// function calculator(num1, num2, oper){
+//     switch (oper){
+//         case "add":
+//             console.log(`yigindisi ${num1 + num2}`);
+//             break;
+//         case "subtract":
+//             console.log(`ayirmasi ${num1 - num2}`);
+//             break;
+//         default:
+//             console.log(`aniqlanmagan amal`)
+//     }
+   
+//     // return(num1, num2, oper)
+// }
+// calculator(12, 4, "subtract")
+
+
+// const getLargest = (son) => {
+//     let largest = son[0];
+//     for( let i = 1; i < son.length; i++ ){
+//         if (son[i] > largest){
+//             largest = son[i]
+//         }    
+//     }
+//     return largest;
+// }
+// const abs = ["213", "245"]
+// console.log(getLargest(abs))
+
+
+
+
+
+
+// function myFunction() {
+//     let name = prompt('Enter your name:');
+    
+//     do {
+//       name = prompt('Enter your name again carefully:');
+//     } while (name !== 'Shukurullo');
+    
+//     alert('Well done, ' + name + '!');
+    
+//     function yosh(age) {
+//       if (age <= 18) {
+//         alert("You can learn programming.");
+//       } else {
+//         alert('You would have already learned IT.');
+//       }
+//     }
+    
+//     let age = parseInt(prompt('Enter your age:'));
+//     while (isNaN(age)) {
+//       age = parseInt(prompt('Please enter a valid age:'));
+//     }
+    
+//     yosh(age);
+    
+//     function square(num) {
+//       return num * num;
+//     }
+    
+//     console.log(square(123));
+//   }
+  
+//   myFunction();
+
+
+
+
+  
